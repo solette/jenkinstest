2e65ae0e1b379ea7f7261db153429e50fd5c3752
Ahmad Jaber <ahmad.jaber@arbetsformedlingen.se>
2020-03-31 22:57:28 +0200

    Extrnal call

---
+++ vars/releasenotes.groovy
@@ -0,0 +1,18 @@
+import java.io.*;
+import groovy.io.*;
+
+def call(Map config=[:]) {
+    // "C:\\repository\\private\\jenkinslab\\jenkinstest\\ConsoleApp1"
+    def dir = new File(pwd);
+    new File( "${dir.path}/releasenotes.txt" ).withWriter('utf-8'){
+    	writer ->
+    	dir.eachFileRecurse {
+    		file ->
+    		if (file.isDirectory()) {
+    		    writer.writeLine("${file.name}")
+    		} else {
+    		    writer.writeLine("\t${file.name}\t${file.length()}")
+    		}
+    	}
+    }
+}
